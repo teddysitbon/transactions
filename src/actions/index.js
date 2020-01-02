2b108e400f9cf713c5ab6406de195d07e46e5b9d
teddysitbon <teddysitbon@gmail.com>
2020-01-02 22:55:24 +0100

    little forgetfulness

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
 import * as lydiaProvider from '../data/providers/';
-import * as types from './constants/types.js';
+import * as types from '../constants/types.js';
 
 export function loadTransactions () {
     return async (dispatch) => {
