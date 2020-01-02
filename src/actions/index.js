0711342a9745e07889b80b95b5c50696f1d0ed81
teddysitbon <teddysitbon@gmail.com>
2020-01-02 22:40:11 +0100

    Try to configure jsconfig

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,8 @@
 import * as lydiaProvider from '../data/providers/';
-import * as types from '../constants/types.js';
+import * as types from './constants/types.js';
 
 export function loadTransactions () {
     return async (dispatch) => {
-        dispatch({
-            type: types.LYDIA_LOADING_TRANSACTIONS_STATE,
-            payload: true
-        });
         const newData = await lydiaProvider.loadTransactions();
         dispatch({
             type: types.LYDIA_LOAD_TRANSACTIONS,
@@ -20,10 +16,6 @@ export function loadTransactions () {
 }
 export function loadUsers () {
     return async (dispatch) => {
-        dispatch({
-            type: types.LYDIA_LOADING_USERS_STATE,
-            payload: true
-        });
         const newData = await lydiaProvider.loadUsers();
         dispatch({
             type: types.LYDIA_LOAD_USERS,
